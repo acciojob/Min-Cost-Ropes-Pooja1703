08e762e9c9fa106159b6f7a0c48f171cca2b48ff
Pooja1703 <129085948+Pooja1703@users.noreply.github.com>
2026-09-18 19:01:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
+let cost = 0;
+
+  while(arr.length > 1){
+    arr.sort((a, b) => a - b);
+
+    let sum = arr[0] + arr[1];
+    cost += sum;
+
+    arr.splice(0, 2);
+    arr.push(sum);
+  }
+
+  return cost;
+}
   
 }
 
